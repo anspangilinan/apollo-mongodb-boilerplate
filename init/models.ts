f3e1e382fe2e8ca75ecac793944af76512726124
anspangilinan <anspangilinan@gmail.com>
2023-12-02 14:31:26 +0800

    improve mongoose model loader

--- init/models.ts
+++ init/models.ts
@@ -1,14 +1,41 @@
 import fs from "fs";
+import path from "path";
 
 export const preloadModels = () => {
-  const modelsPath = __dirname + "/../src/data/models";
-  fs.readdirSync(modelsPath).forEach(function (file) {
-    if (file == "constants" || file.indexOf(".md")) {
-      return;
-    }
-    if (-file.indexOf(".ts")) {
-      require(modelsPath + "/" + file);
-    }
+  const modelsPath = path.resolve(__dirname, "../src/data/models");
+
+  const loadedModels: { [key: string]: string[] } = {};
+  const loadModels = (dirPath: string, relativePath: string = "") => {
+    fs.readdirSync(dirPath).forEach((file) => {
+      const fullPath = path.join(dirPath, file);
+      const isModelFile: boolean =
+        fs.lstatSync(fullPath).isFile() && file.endsWith(".model.ts");
+      const isDirectory: boolean = fs.lstatSync(fullPath).isDirectory();
+
+      if (isDirectory) {
+        loadModels(fullPath, path.join(relativePath, file));
+      } else if (isModelFile) {
+        const models = require(fullPath);
+
+        Object.keys(models).forEach((modelName) => {
+          const key = path.join(relativePath, file.replace(".model.ts", ""));
+          loadedModels[key] = loadedModels[key] || [];
+          loadedModels[key].push(modelName);
+        });
+      }
+    });
+  };
+
+  loadModels(modelsPath);
+  console.log("\n============================================");
+  console.log("Loaded the following Mongoose models:");
+  const perModelDelimiter = "\n - ";
+  Object.keys(loadedModels).forEach((key) => {
+    console.log(
+      `\n${key}${perModelDelimiter}${loadedModels[key]
+        .sort()
+        .join(perModelDelimiter)}`
+    );
   });
-  console.log("Mongoose Models are loaded to MongoDB");
+  console.log("\n============================================\n");
 };
